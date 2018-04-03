0cc78c2ae98d8a09426df9822bd659013d513101
Sahana Balram <sahana2008balram@gmail.com>
2018-04-03 16:26:07 -0700

    Import Search Schema

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Bing = require("node-bing-api")({apiKey:"03b345b6eac047718232317ede3f7bae"});
+// require the schema
+const search = require("./models/search");
 const PORT = process.env.port || 3001;
 app.use(express.static('app/public'));
 app.use(bodyParser.json());
@@ -11,12 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 app.use(cors());
-
+// connect to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/searches");
 app.get("/api/imagesearch/:searchValue*",function(req,res){
     let searchValue = req.params.searchValue;
     let offset = req.query.offset;
 
-    res.json({searchValue,offset})
+    return res.json({searchValue,offset})
 });
 app.listen(PORT, function () {
     console.log("App is listening on:" + PORT);
