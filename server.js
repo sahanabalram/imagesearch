2b7ab5f73cae58d5926db83524799320eab902bd
Sahana Balram <sahana2008balram@gmail.com>
2018-04-03 16:14:49 -0700

    Add bing search api details

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+const mongoose = require("mongoose");
+const Bing = require("node-bing-api")({apiKey:"03b345b6eac047718232317ede3f7bae"});
 const PORT = process.env.port || 3001;
 app.use(express.static('app/public'));
 app.use(bodyParser.json());
@@ -10,6 +12,12 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 app.use(cors());
 
+app.get("/api/imagesearch/:searchValue*",function(req,res){
+    let searchValue = req.params.searchValue;
+    let offset = req.query.offset;
+
+    res.json({searchValue,offset})
+});
 app.listen(PORT, function () {
     console.log("App is listening on:" + PORT);
 })
